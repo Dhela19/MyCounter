0fb28bcb105b3db04e15861523ed3c5616437244
Ubongabasi Jerome <87214555+Dhela19@users.noreply.github.com>
2023-01-21 07:20:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var n = (document.getElementById("number"));
+
+function increase(){n.innerHTML=n.innerHTML-0+1};
+
+function decrease(){n.innerHTML=n.innerHTML-1
+};
+
+function reset()
+{
+    n.innerHTML=0
+}
